f7b26901b0af299711d1979fb9baeba602645176
Lokesh Datta Ojha <lojha429@gmail.com>
2024-03-25 20:17:53 +0545

    New Commit

--- ViewData/obj/Release/netcoreapp3.0/ViewData.AssemblyInfo.cs
+++ ViewData/obj/Release/netcoreapp3.0/ViewData.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewData")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd140626dc4c3f88cdfbf62b1c19c9da5c9cb7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewData")]
